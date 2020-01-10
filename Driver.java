74791ac7b95602d53497b2fde1f0ccb2a33e2f71
agabrie <abduraghmaangabriels2@gmail.com>
2020-01-10 15:42:47 +0200

    can rotate using formatted instructions

--- Driver.java
+++ Driver.java
@@ -1,25 +1,78 @@
 import java.util.Scanner;
 class Driver{
+    // public currentBase
+    static Rubik cube = new Rubik();
     public static void main(String [] args){
         // Face face =  new Face(Color.BLUE,Orientation.FRONT);
-        Rubik cube = new Rubik();
         // try{
         Scanner scInput = new Scanner(System.in);
         boolean clockwise = true;
-        int face;
+        // int face;
+        String instr;
+        boolean extra = false;
         String line;
         while(scInput.hasNext()){
             line = scInput.nextLine();
             // System.out.println(line);
             Scanner scLine = new Scanner(line).useDelimiter(" ");
             while(scLine.hasNext()){
-                face = scLine.nextInt();
-                face = face % 6;
-                clockwise = scLine.next().equals("cw");
-                cube.rotateFace(clockwise, face);
+                // face = scLine.nextInt();
+                // face = face % 6;
+                instr = scLine.nextLine();
+                System.out.println("Instruction : "+instr);
+                if(instr.length() > 1){
+                    clockwise = (!instr.contains("\'"));
+                    extra = (instr.contains("2"));
+                }
+                // System.out.println("something");
+                switch(instr.charAt(0)){
+                    case 'F':
+                        F(clockwise,extra);
+                        break;
+                    case 'B':
+                        B(clockwise,extra);
+                        break;
+                    case 'L':
+                        L(clockwise,extra);
+                        break;
+                    default:
+                        break;
+                }
+
+                // cube.rotateFace(clockwise, face);
                 System.err.print(cube.toString());
             }
         }
+    }
+    public static void F(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 0);
+        if(extra)
+            cube.rotateFace(clockwise, 0);
+    }
+    public static void D(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 1);
+        if(extra)
+            cube.rotateFace(clockwise, 1);
+    }
+    public static void R(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 2);
+        if(extra)
+            cube.rotateFace(clockwise, 2);
+    }
+    public static void L(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 3);
+        if(extra)
+            cube.rotateFace(clockwise, 3);
+    }
+    public static void B(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 4);
+        if(extra)
+            cube.rotateFace(clockwise, 4);
+    }
+    public static void U(boolean clockwise,boolean extra){
+        cube.rotateFace(clockwise, 5);
+        if(extra)
+            cube.rotateFace(clockwise, 5);
     }
         // catch();
         // System.out.println(cube);
